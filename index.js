478a203e3e1d8d7f150df43e20f4e29f8ec6b2bf
Janumala Akhilendra <82641474+JanumalaAkhilendra@users.noreply.github.com>
2022-06-13 19:33:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // function to validate to users
-var username1 = document.getElementById('username').values
-var password1 = document.getElementById('password').values
-function validate(){
+
+function validate() {
+    var username1 = document.getElementById('username').value
+    var password1 = document.getElementById('password').value
+    // console.log(username1);
+    // console.log(password1);
     console.log('clicked');
-    if(password1 ===''){
+    if (username1 == 'admin' && password1 == 'user') {
         alert('Login Sucessfully');
-         return false;
-    }else{
+        return false;
+    } else {
         alert('Login Failed');
     }
 }
